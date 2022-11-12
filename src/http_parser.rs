f8653bd2cdeba4f4ceddaa46d91a1ebe05d1ab8f
osawa-koki <github@osawa.dev>
2022-11-12 18:02:39 +0900

    スタイル調整。

--- src/http_parser.rs
+++ src/http_parser.rs
@@ -1,44 +1,44 @@
 
 
 pub mod http_parser {
-    //use std::io::BufReader;
-    //use std::net::TcpStream;
-    use std::io::BufRead;
-    use std::{io::BufReader, net::TcpStream};
+  //use std::io::BufReader;
+  //use std::net::TcpStream;
+  use std::io::BufRead;
+  use std::{io::BufReader, net::TcpStream};
 
 
-    #[derive(Debug)]
-    pub enum HttpMethod {
-        Get,
-        Post,
-        Unknown,
+  #[derive(Debug)]
+  pub enum HttpMethod {
+    Get,
+    Post,
+    Unknown,
+  }
+
+  pub struct HttpRequestStruct {
+    pub method: HttpMethod,
+    pub path: String,
+  }
+
+
+  pub fn parse(stream: TcpStream) -> HttpRequestStruct {
+    let mut stream_reader = BufReader::new(stream);
+
+    let mut first_line = String::new();
+    if let Err(err) = stream_reader.read_line(&mut first_line) {
+      panic!("error during receive a line: {}", err);
     }
 
-    pub struct HttpRequestStruct {
-        pub method: HttpMethod,
-        pub path: String,  
-    }
+    let mut params = first_line.split_whitespace();
+    let method = params.next().unwrap();
+    let path = params.next().unwrap();
 
+    let http_request_data = HttpRequestStruct {
+      method: if method == "GET" {HttpMethod::Get} else if method == "POST" {HttpMethod::Post} else {HttpMethod::Unknown},
+      path: path.to_string(),
+    };
 
-    pub fn parse(stream: TcpStream) -> HttpRequestStruct {
-        let mut stream_reader = BufReader::new(stream);
-    
-        let mut first_line = String::new();
-        if let Err(err) = stream_reader.read_line(&mut first_line) {
-            panic!("error during receive a line: {}", err);
-        }
-    
-        let mut params = first_line.split_whitespace();
-        let method = params.next().unwrap();
-        let path = params.next().unwrap();
-
-        let http_request_data = HttpRequestStruct {
-            method: if method == "GET" {HttpMethod::Get} else if method == "POST" {HttpMethod::Post} else {HttpMethod::Unknown},
-            path: path.to_string(),
-        };
-
-        return http_request_data;
-    }
+    return http_request_data;
+  }
 
 }
 
--- src/tester.rs
+++ src/tester.rs
@@ -7,35 +7,35 @@ use std::net::TcpListener;
 mod http_parser;
 
 fn main() {
-	dotenv().ok();
+  dotenv().ok();
 
-	let ip_address = env::var("IP_ADDRESS").expect("IP_ADDRESS is not defined");
-	let http_port = env::var("HTTP_PORT").expect("HTTP_PORT is not defined");
-	println!("{}", format!("IP_ADDRESS -> {}", ip_address));
-	println!("{}", format!("HTTP_PORT -> {}", http_port));
-	tcp_listener(ip_address, http_port)
+  let ip_address = env::var("IP_ADDRESS").expect("IP_ADDRESS is not defined");
+  let http_port = env::var("HTTP_PORT").expect("HTTP_PORT is not defined");
+  println!("{}", format!("IP_ADDRESS -> {}", ip_address));
+  println!("{}", format!("HTTP_PORT -> {}", http_port));
+  tcp_listener(ip_address, http_port)
 }
 
 
 fn tcp_listener(ip_address: String, http_port: String) {
-	let listener = TcpListener::bind(format!("{}:{}", ip_address, http_port)).unwrap();
+  let listener = TcpListener::bind(format!("{}:{}", ip_address, http_port)).unwrap();
 
-	for stream in listener.incoming() {
-		handle_connection(stream.unwrap());
-	}
+  for stream in listener.incoming() {
+    handle_connection(stream.unwrap());
+  }
 }
 
 
 fn handle_connection(mut stream: TcpStream) {
-	let cloned_stream = stream.try_clone();
-	
-	let http_request_data = http_parser::http_parser::parse(cloned_stream.unwrap());
+  let cloned_stream = stream.try_clone();
 
-	println!("{}", format!("MATHOD -> {:?}", http_request_data.method));
-	println!("{}", format!("MATHOD -> {:?}", http_request_data.path));
+  let http_request_data = http_parser::http_parser::parse(cloned_stream.unwrap());
 
-	let response = "HTTP/1.1 200 OK\r\n\r\nHELLO";
-	stream.write(response.as_bytes()).unwrap();
-	stream.flush().unwrap();
+  println!("{}", format!("MATHOD -> {:?}", http_request_data.method));
+  println!("{}", format!("MATHOD -> {:?}", http_request_data.path));
+
+  let response = "HTTP/1.1 200 OK\r\n\r\nHELLO";
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
 }
 
